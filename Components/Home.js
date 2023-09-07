d67d7b5e0120c9032eada654ccc0274e69dcfdb8
hassaan-shafique <hassaanshafique398@gmail.com>
2023-09-07 17:19:47 +0500

    Cards

--- Components/Home.js
+++ Components/Home.js
@@ -29,31 +29,65 @@ const Home = () => {
         <span className='text-red-700'>TRUE </span>  <br/> <br/> <br/>
          STYLE
           HERE</p>
-<br/>
+             <br/>
         <button type='submit' className="w-[16%] mt-14 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ">
           Buy Now
         </button> 
       </div>
          <hr className="mt-[6%]"/>
+         <br/>
       <div>
-        <h1 style={{fontSize:'45px'}} className=' flex justify-center text-bold mb-2text-xl'> TRENDING 
+        <h1 style={{fontSize:'45px'}} className=' flex justify-center text-bold mb-6 text-xl'> TRENDING 
         </h1>
-        <div className="max-w-sm rounded overflow-hidden shadow-lg
-        ">
-       
-         <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
+           <br/>
+        <div className='flex justify-center space-between space-x-[3%]'>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <Image 
+               src="/Hoodie.jpg"
+               alt='hoodies'
+               height={400}
+               width={400}
+                 />
 
-        </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Hoodies</div>
+                       <button className='text-white bg-blue-600 rounded-full ml-[29%] py-3 px-3'>Buy Now</button>
+                     </div>
+               </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <Image 
+               src="/Pant2.jpg"
+               alt='hoodies'
+               height={400}
+               width={400}
+                className='h-[61%]'
+                 />
+
+                <div class="px-6 py-3">
+                    <div class="font-bold text-xl mb-2">Pants</div>
+                        <button className='text-white bg-blue-600 rounded-full ml-[29%] py-3 px-3'>Buy Now</button>
+                     </div>
+               </div>
+                <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <Image 
+               src="/shirts.jpg"
+               alt='hoodies'
+               height={400}
+               width={400}
+              
+                 />
+
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">shirts</div>
+                         <button className='text-white bg-blue-600 rounded-full ml-[29%] py-3 px-3'>Buy Now</button>
+                     </div>
+               </div>
+           </div>
+        <br/>
+        <hr className="mt-[6%]"/>
+
       </div>
     </div>
-
-
-    
   )
  
 }
