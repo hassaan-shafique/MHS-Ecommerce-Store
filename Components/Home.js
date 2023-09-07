6909d00561602d36ed77bcb8fc72e4f5b0ea1932
hassaan-shafique <hassaanshafique398@gmail.com>
2023-09-07 12:41:50 +0500

    add image

--- Components/Home.js
+++ Components/Home.js
@@ -9,24 +9,33 @@ const Home = () => {
      
     <div>
      <Image
-      className=" border-rounded ml-[70%] my-6 rounded-lg shadow-md my-7"
+      className=" border-rounded ml-[60%] my-6 rounded-lg shadow-md my-7"
      src ="/image4.png" 
      width ={140}
      height ={140}
      alt ="front image"
      />
+      <Image
+      className=" border-rounded  mt-[-420px] ml-[75%] my-5 rounded-lg shadow-md my-8"
+     src ="/image3.png" 
+     width ={140}
+     height ={140}
+     alt ="front image"
+     />
+
         <div className="flex flex-col mt-[-400px] ml-14 ">
         <p style={{ fontSize: '48px' }} className="text-xl font-semibold">FIND 
         YOUR <br/> <br/><br/> 
-         TRUE  <br/> <br/> <br/>
+        <span className='text-red-700'>TRUE </span>  <br/> <br/> <br/>
          STYLE
           HERE</p>
 <br/>
-        <button type='submit' className="w-16 bg-blue-600 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600">
+        <button type='submit' className="w-[16%] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ">
           Buy Now
         </button> 
       </div>
     </div>
+
 
     
   )
