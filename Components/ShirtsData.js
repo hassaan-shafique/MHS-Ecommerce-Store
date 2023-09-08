5887083f9a30502bc320d976b84cd7242c0f98a3
hassaan-shafique <hassaanshafique398@gmail.com>
2023-09-08 15:30:26 +0500

    Shirt Data

--- Components/ShirtsData.js
+++ Components/ShirtsData.js
@@ -1,56 +1,63 @@
 
-// import { images } from "@/next.config";
+import { images } from "@/next.config";
 
 
-// const ShirtsData  = [
-//     {
-//         id:'1',
-//         name :'Hoodie 1',
-//         price : 1000,
-//         description: 'Cotton Shirt',
-//         image:"./"
+const ShirtsData  = [
+    {
+        id:'1',
+        name :'Hoodie 1',
+        price : 1000,
+        description: 'Cotton Shirt',
+        image:"./"
 
-//     },
-//     {
-//          id:'2',
-//         name :'Shirt 2',
-//         price : 1000,
-//         description: 'Cotton Shirt',
-//         image:"./"
+    },
+    {
+         id:'2',
+        name :'Shirt 2',
+        price : 1000,
+        description: 'Cotton Shirt',
+        image:"./"
 
-//     },
-//     {
-//           id:'3',
-//         name :'Shirt 3',
-//         price : 1000,
-//         description: 'Cotton Shirt',
-//         image:"./"
+    },
+    {
+          id:'3',
+        name :'Shirt 3',
+        price : 1000,
+        description: 'Cotton Shirt',
+        image:"./"
 
-//     },
-//     {
-//           id:'4',
-//         name :'Shirt 4',
-//         price : 1000,
-//         description: 'Cotton Shirt',
-//         image:"./"
-//     },
-//     {
-//           id:'5',
-//         name :'Shirt 5',
-//         price : 1000,
-//         description: 'Cotton Shirt',
-//         image:"./"
+    },
+    {
+          id:'4',
+        name :'Shirt 4',
+        price : 1000,
+        description: 'Cotton Shirt',
+        image:"./"
+    },
+    {
+          id:'5',
+        name :'Shirt 5',
+        price : 1000,
+        description: 'Cotton Shirt',
+        image:"./"
 
-//     },
-//     {
+    },
+    {
 
-//           id:'6',
-//         name :'Shirt 6',
-//         price : 100,
-//         description: 'Cotton Shirt',
-//         image:"./"
-//     }
+          id:'6',
+        name :'Shirt 6',
+        price : 1000,
+        description: 'Cotton Shirt',
+        image:"./"
+    },
+ {
 
+          id:'7',
+        name :'Shirt 7',
+        price : 1000,
+        description: 'Cotton Shirt',
+        image:"./"
+    }
 
-// ]
-// export default ShirtsData;
+]
+export default ShirtsData;
