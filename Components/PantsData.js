9eb002aef1f3b1af7b4d2e4eba174e1e121946d3
hassaan-shafique <hassaanshafique398@gmail.com>
2023-09-08 15:28:37 +0500

    Pants Data

---
+++ Components/PantsData.js
@@ -0,0 +1,55 @@
+import { images } from "@/next.config";
+
+
+const PantsData  = [
+    {
+        id:'1',
+        name :'Pant 1',
+        price : 1200,
+        description: 'Denim Pant',
+        image:"./"
+
+    },
+    {
+         id:'2',
+        name :'Pant 2',
+        price : 1200,
+        description: 'Denim Pant',
+        image:"./"
+
+    },
+    {
+          id:'3',
+        name :'Pant 3',
+        price : 1200,
+        description: 'Denim pant',
+        image:"./"
+
+    },
+    {
+          id:'4',
+        name :'Pant 4',
+        price : 1200,
+        description: 'Denim Pant',
+        image:"./"
+    },
+    {
+          id:'5',
+        name :'Pant 5',
+        price : 1200,
+        description: 'Denim Pant',
+        image:"./"
+
+    },
+    {
+
+          id:'6',
+        name :'Pant 6',
+        price : 1200,
+        description: 'Denim Pant',
+        image:"./"
+    }
+
+
+]
+export default PantsData;
--- Components/ShirtsData.js
+++ Components/ShirtsData.js
@@ -1,1 +1,56 @@
 
+// import { images } from "@/next.config";
+
+
+// const ShirtsData  = [
+//     {
+//         id:'1',
+//         name :'Hoodie 1',
+//         price : 1000,
+//         description: 'Cotton Shirt',
+//         image:"./"
+
+//     },
+//     {
+//          id:'2',
+//         name :'Shirt 2',
+//         price : 1000,
+//         description: 'Cotton Shirt',
+//         image:"./"
+
+//     },
+//     {
+//           id:'3',
+//         name :'Shirt 3',
+//         price : 1000,
+//         description: 'Cotton Shirt',
+//         image:"./"
+
+//     },
+//     {
+//           id:'4',
+//         name :'Shirt 4',
+//         price : 1000,
+//         description: 'Cotton Shirt',
+//         image:"./"
+//     },
+//     {
+//           id:'5',
+//         name :'Shirt 5',
+//         price : 1000,
+//         description: 'Cotton Shirt',
+//         image:"./"
+
+//     },
+//     {
+
+//           id:'6',
+//         name :'Shirt 6',
+//         price : 100,
+//         description: 'Cotton Shirt',
+//         image:"./"
+//     }
+
+
+// ]
+// export default ShirtsData;
